e71f2092945654438819a723bb68e8b122bf2df8
Andre Bagrovsky <bav55@yandex.ru>
2018-11-28 15:35:37 +0600

    Фикс: отображение контактных телефонов на главной странице

--- resources/views/view/blocks/phones.blade.php
+++ resources/views/view/blocks/phones.blade.php
@@ -4,12 +4,12 @@ $phones = json_decode(\App\Models\Setting::all()->find(1)->phone);
 <div class="phones-holder">
     <div class="main-phone">
         @foreach($phones as $phone)
-            @if ($phocount == 1)
-                <a href="tel:{{ $phone->number }}">{!! $phone->visual !!}</a>
-            @endif
-            @php $phocount++; @endphp
-        @endforeach
-        <!--<i class="phones-handler"></i>-->
+
+            <a href="tel:{{ $phone->number }}">{!! $phone->visual !!}</a>
+
+        @php $phocount++; @endphp
+    @endforeach
+    <!--<i class="phones-handler"></i>-->
     </div>
     <ul class="phones-list">
         @foreach($phones as $phone)
